9f0711d2321b47ae77f2c20a0e3bb5ed8fa0150c
Henrique Augusto de Oliveira <haugustodeoliveira@gmail.com>
2020-10-28 19:48:50 -0300

    Definicao dos comandos que a sonda pode executar

---
+++ ProjectMars/src/Controller/Commands.java
@@ -0,0 +1,11 @@
+package Controller;
+
+/**
+ * R: comando para girar a Sonda em 90 graus para direita 
+ * L: comando para girar a Sonda em 90 graus para esquerda
+ * M: comando para avançar um ponto no plano
+ */
+
+public enum Commands {
+	R,L,M;
+}
